709feb3fa09fcbc04154a9d60e02f376d16d6991
Luciano Carril Reyes <luciano.carril@usach.cl>
2025-05-20 17:30:05 -0400

    TDA jugador

---
+++ TDA_jugador.pl
@@ -0,0 +1,3 @@
+% jugador(Id, Nombre, Dinero, Propiedades, PosicionActual, EstaEnCarcel, TotalCartasSalirCarcel, Jugador)
+jugador(Id, Nombre, Dinero, Propiedades, PosicionActual, EstaEnCarcel, TotalCartasSalirCarcel,
+        [Id, Nombre, Dinero, Propiedades, PosicionActual, EstaEnCarcel, TotalCartasSalirCarcel]).
