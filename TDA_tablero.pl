129885828f7c419058501d16a8e195fa39721034
Luciano Carril Reyes <luciano.carril@usach.cl>
2025-05-21 17:07:58 -0400

    agrego RF07

--- TDA_tablero.pl
+++ TDA_tablero.pl
@@ -1,2 +1,8 @@
 %propiedades (lista) X cartas-suerte (lista) X cartas-comunidad (lista) X casillas-especiales (lista) X tablero (board)
 tablero(Propiedades,CartasSuerte,CartaComunidad,CasillasEspeciales,[Propiedades,CartasSuerte,CartaComunidad,CasillasEspeciales]).
+
+%necesito hacer un append a la parte de propiedades y que me devuelva el tablero modificado
+tableroAgregarPropiedades(TableroIN, Propiedades, TableroOUT) :-
+    tablero(PropiedadesIN, CartasSuerte, CartaComunidad, CasillasEspeciales, TableroIN),
+    append(PropiedadesIN, Propiedades, PropiedadesOUT),
+    tablero(PropiedadesOUT, CartasSuerte, CartaComunidad, CasillasEspeciales, TableroOUT).
