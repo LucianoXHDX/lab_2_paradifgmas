c22a677839b4a33157d37af49c103515b1ee87dc
Luciano Carril Reyes <luciano.carril@usach.cl>
2025-05-24 00:43:55 -0400

    SOLO ME ESTA ENTREGANDO EL INDICE; NECESITO QUE ME ENTREGE EL JUGADORRRR

--- TDA_juego.pl
+++ TDA_juego.pl
@@ -1,4 +1,4 @@
-use_module('TDAjugador.pl').
+:-use_module('TDAjugador.pl').
 
 
 
@@ -6,8 +6,11 @@ use_module('TDAjugador.pl').
 juego(Jugadores,Tablero,DineroBanco,NumeroDados,TurnoActual,TasaImpuesto,MaximoCasas,MaximoHoteles,
 	[Jugadores,Tablero,DineroBanco,NumeroDados,TurnoActual,TasaImpuesto,MaximoCasas,MaximoHoteles]).
 %Agregar jugadadres
-juegoAgregarJugadores([JugadoresIn, Tablero, DineroBanco, Dados, Turno, TasaImpuesto, MaxCasas, MaxHoteles],JugadorNuevo,
-    [JugadoresOut, Tablero, NuevoDineroBanco, Dados, Turno, TasaImpuesto, MaxCasas, MaxHoteles]):-
+juegoAgregarJugadores([JugadoresIn, Tablero, DineroBanco, Dados, Turno, TasaImpuesto, MaxCasas, MaxHoteles],JugadorNuevo,[JugadoresOut, Tablero, NuevoDineroBanco, Dados, Turno, TasaImpuesto, MaxCasas, MaxHoteles]):-
     jugadorNuevoDinero(JugadorNuevo, 1500, JugadorConDinero),
     append(JugadoresIn, [JugadorConDinero], JugadoresOut),
     NuevoDineroBanco is DineroBanco - 1500.
+
+%%getter de juego TurnoActual
+juegoObtenerTurnoActual([_,_,_,_,TurnoActual,_,_,_],TurnoActual).
+%me esta entregando en indice, me falta que me buscar el jugador y que me entrege el jugador.
