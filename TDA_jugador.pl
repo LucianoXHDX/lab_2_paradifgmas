b4dd361b98bc5d6ae9c53cafb950e2ad1c5c4b73
Luciano Carril Reyes <luciano.carril@usach.cl>
2025-06-01 02:42:10 -0400

    repito mi subida ya que no habia guardado antes de subir

--- TDA_jugador.pl
+++ TDA_jugador.pl
@@ -11,15 +11,49 @@
 
 jugador(Id, Nombre, Dinero, Propiedades, PosicionActual, EstaEnCarcel, TotalCartasSalirCarcel,
         [Id, Nombre, Dinero, Propiedades, PosicionActual, EstaEnCarcel, TotalCartasSalirCarcel]).
-
 %setter de dinero, propiedades, posicion actual esta en carcel y total de cartas
 %setter dinero
+
+%Descripcion:
+%Dominio:
+%Recorrido:
+%Tipo de algoritmo:
 jugadorGetId([Id|_],Id).
+
+%Descripcion:
+%Dominio:
+%Recorrido:
+%Tipo de algoritmo:
 jugadorGetNombre([_,Nombre|_],Nombre).
+
+%Descripcion:
+%Dominio:
+%Recorrido:
+%Tipo de algoritmo:
 jugadorGetDinero([_,_,Dinero|_],Dinero).
+
+%Descripcion:
+%Dominio:
+%Recorrido:
+%Tipo de algoritmo:
 jugadorGetPropiedades([_,_,_,Propiedades|_],Propiedades).
+
+%Descripcion:
+%Dominio:
+%Recorrido:
+%Tipo de algoritmo:
 jugadorGetPosicionActual([_,_,_,_,PosicionActual|_],PosicionActual).
+
+%Descripcion:
+%Dominio:
+%Recorrido:
+%Tipo de algoritmo:
 jugadorGetEstaEnCarcel([_,_,_,_,_,EstaEnCarcel|_],EstaEnCarcel).
+
+%Descripcion:
+%Dominio:
+%Recorrido:
+%Tipo de algoritmo:
 jugadorGetTotalCartasSalirCarcel([_,_,_,_,_,_,TotalCartasSalirCarcel|_],TotalCartasSalirCarcel).
 
 
@@ -29,14 +63,27 @@ jugadorGetTotalCartasSalirCarcel([_,_,_,_,_,_,TotalCartasSalirCarcel|_],TotalCar
 
 %finGetter
 
+%Descripcion:
+%Dominio:
+%Recorrido:
+%Tipo de algoritmo:
 jugadorSetDinero([Id, Nombre, Dinero, Propiedades, PosicionActual, EstaEnCarcel, TotalCartasSalirCarcel],
                  Cambio,
                  [Id, Nombre, NuevoDinero, Propiedades, PosicionActual, EstaEnCarcel, TotalCartasSalirCarcel]) :-
     NuevoDinero is Dinero + Cambio.
 
 
+%Descripcion:
+%Dominio:
+%Recorrido:
+%Tipo de algoritmo:
 jugadorNuevoDinero([Id, Nombre, _, Propiedades, PosicionActual, EstaEnCarcel, TotalCartasSalirCarcel], NuevaPlata,[Id, Nombre, NuevaPlata, Propiedades, PosicionActual, EstaEnCarcel, TotalCartasSalirCarcel]).
  %setter Mover jugador y que se sume su posicion
+
+%Descripcion:
+%Dominio:
+%Recorrido:
+%Tipo de algoritmo:
 jugadorSumarPosicion([Id,Nombre,Dinero,Propiedades,PosicionActual,EstaEnCarcel,TotalCartasSalirCarcel],
                         CantAvanza,
                         [Id,Nombre,Dinero,Propiedades,NuevaPosicion,EstaEnCarcel,TotalCartasSalirCarcel]):-
@@ -46,6 +93,11 @@ jugadorSumarPosicion([Id,Nombre,Dinero,Propiedades,PosicionActual,EstaEnCarcel,T
 
 
 %hacer con setters
+
+%Descripcion:
+%Dominio:
+%Recorrido:
+%Tipo de algoritmo:
 jugadorComprarPropiedad(JugadorIn,PropiedadIn,PropiedadOut,JugadorOut):-
         jugador(IdJugador, NombreJugador, Dinero, Propiedades, PosicionActual, EstaEnCarcel, TotalCartasSalirCarcel,JugadorIn),
         propiedad(IdPropiedad,NombrePropiedad,Precio,Renta,_Duenno,Casas,EsHotel,EstaHipotecada,PropiedadIn),
@@ -56,21 +108,12 @@ jugadorComprarPropiedad(JugadorIn,PropiedadIn,PropiedadOut,JugadorOut):-
         append(Propiedades,[IdPropiedad],NuevasPropiedades),
         jugador(IdJugador, NombreJugador, NuevoDinero, NuevasPropiedades, PosicionActual, EstaEnCarcel, TotalCartasSalirCarcel,JugadorOut). 
 %verificar bancarrota
+
+%Descripcion:
+%Dominio:
+%Recorrido:
+%Tipo de algoritmo:
 jugadorEstaEnBancarrota(JugadorIn):-
         jugadorGetDinero(JugadorIn,DineroJugador),
         DineroJugador =< 0.
 %verdadero si esta en bancarrota
-%setter para mover al jugador
-
-
-
-
-/*tableroActualizarPropiedades([],_,[]).% caso base
-
-tableroActualizarPropiedades([PrimeraPropiedad|RestoPropiedades],PropiedadIn,[PropiedadIn|RestoPropiedades]):-
-  propiedadGetId(PropiedadIn,IdPropiedadIn),
-  propiedadGetId(PrimeraPropiedad,IdPrimeraPropiedad),
-  IdPrimeraPropiedad =:= IdPropiedadIn, !.
-
-tableroActualizarPropiedades([PrimeraPropiedad|RestoPropiedades],PropiedadIn,[PrimeraPropiedad|ListaPropiedadesActualizadas]):-
-  tableroActualizarPropiedades(RestoPropiedades,PropiedadIn,ListaPropiedadesActualizadas).*/
